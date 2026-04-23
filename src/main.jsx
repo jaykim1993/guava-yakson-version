25282140140b501281b7d80f615dad43704ee0f5
jaykim1993 <tjdwnddl809@naver.com>
2026-04-23 22:00:10 +0900

    tis

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Vite가 vite.config.js의 base 설정을 알아서 가져와서 넣어줍니다 */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
